21dd00bba9d5a28b59ee539e260a62df2c2d0cb1
Dominik Haitz <dhaitz@users.noreply.github.com>
2019-11-15 12:59:02 +0100

    Update setup.py

--- {{cookiecutter.package_name}}/setup.py
+++ {{cookiecutter.package_name}}/setup.py
@@ -15,7 +15,11 @@ setup(
     author_email="{{ cookiecutter.author_email }}",
 
     description="{{ cookiecutter.package_description }}",
+{%- if cookiecutter.add_extended_documentation %}
     long_description=Path('README.rst').open().read(),
+{% else -%}
+    long_description=Path('README.md').open().read(),
+{% endif -%}
 
     packages=["{{ cookiecutter.package_name }}"],
 
